a418e08e5d8cce4053ea049380997c4d0ca33959
Marcus Henrique <djfearme@gmail.com>
2017-06-04 13:56:45 -0300

    primeiro commit

---
+++ src/com/marcus/rain/entity/Entity.java
@@ -0,0 +1,32 @@
+package com.marcus.rain.entity;
+
+import java.util.Random;
+
+import com.marcus.rain.graphics.Screen;
+import com.marcus.rain.level.Level;
+
+public abstract class Entity {
+
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen){
+		
+	}
+	
+	public void remove() {
+		//Remove from level.
+		removed = true;
+	}
+	
+	public boolean isRemoved(){
+		return removed;
+	}
+	
+}
